8db285b90108d7a9c9bbfa4f526565d2aa026f20
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-11 17:19:47 +0900

    Update l-tike_mailaddres.js

--- l-tike_mailaddres.js
+++ l-tike_mailaddres.js
@@ -1,2 +1,2 @@
-document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-9)+"@biglode.jp";
+document.forms.ttg160.elements.MAIL_ADDRS.value=document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-9)+"@biglode.jp";
 document.forms.ttg160.elements.TEL.value=document.forms.ttg160.elements.TEL_CONFIRM.value="080"+(Math.floor(Math.random()*Math.floor(80000000))+10000000);
