eeb50842d3139c4976153782f6470e080eb6b758
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-12 00:21:33 +0900

    Update l-tike_namae.js

--- l-tike_namae.js
+++ l-tike_namae.js
@@ -13,7 +13,7 @@ var mei = array_mei[mei_pic];
 var sei_kana = array_sei_kana[sei_pic];
 var mei_kana = array_mei_kana[mei_pic];
 
-document.forms.ttg100.elements.PWD.value = document.forms.ttg100.elements.PWD_CNF.value = 
+document.forms.ttg100.elements.PWD.value = document.forms.ttg100.elements.PWD_CNF.value = Math.random().toString(36).slice(-4);
 document.forms.ttg100.elements.APLCT_FIRST_NAME.value = sei;
 document.forms.ttg100.elements.APLCT_LAST_NAME.value = mei;
 document.forms.ttg100.elements.APLCT_FIRST_NAME_KANA.value = sei_kana;
