f33155997af5e8ebb5b736b7a52b6e91c2528c08
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-11 17:41:10 +0900

    Update aa.js

--- aa.js
+++ aa.js
@@ -1,3 +1,3 @@
 document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-9)+"@biglode.jp";
 document.forms.ttg160.elements.TEL.value=document.forms.ttg160.elements.TEL_CONFIRM.value="080"+(Math.floor(Math.random()*Math.floor(80000000))+10000000)%7D();
-//document.querySelector("button[name=NEXT]").click();
+document.querySelector("button[name=NEXT]").click();
