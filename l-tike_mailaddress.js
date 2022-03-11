732562bb1b8a6bee9947a6fe148423b17e45dede
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-11 17:38:35 +0900

    Update l-tike_mailaddress.js

--- l-tike_mailaddress.js
+++ l-tike_mailaddress.js
@@ -1,3 +1,4 @@
-//ローチケメアド電話番号自動入力共用
-//document.forms.ttg160.elements.MAIL_ADDRS.value=document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-9)+"@biglode.jp";
+//ローチケメアド電話番号自動入力[共用]
+
+document.forms.ttg160.elements.MAIL_ADDRS.value=document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-9)+"@biglode.jp";
 document.forms.ttg160.elements.TEL.value=document.forms.ttg160.elements.TEL_CONFIRM.value="080"+(Math.floor(Math.random()*Math.floor(80000000))+10000000);
