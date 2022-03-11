5f14a660422f683a38c15401988deaffa126228c
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-12 02:48:33 +0900

    Update l-tike_mailaddress.js

--- l-tike_mailaddress.js
+++ l-tike_mailaddress.js
@@ -4,4 +4,5 @@ const ketasuu = 10; //メアドの桁数を指定
 
 document.forms.ttg160.elements.MAIL_ADDRS.value=document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-1*ketasuu)+"@biglode.jp";
 document.forms.ttg160.elements.TEL.value=document.forms.ttg160.elements.TEL_CONFIRM.value="080"+(Math.floor(Math.random()*Math.floor(80000000))+10000000);
-document.querySelector("[name=NEXT]").click();
+
+document.querySelector("[name=NEXT]").click(); //次ページへ
