eaf63a85e0c8a82561daefed4fb503457e68aefe
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-12 02:13:47 +0900

    Update l-tike_namae.js

--- l-tike_namae.js
+++ l-tike_namae.js
@@ -12,7 +12,7 @@ const mei_pic = Math.floor(Math.random()*Math.floor(mei_len));
 
 var sei = array_sei[sei_pic];
 var mei = array_mei[mei_pic];
-//var sei_kana = array_sei_kana[sei_pic];
+var sei_kana = array_sei_kana[sei_pic];
 var mei_kana = array_mei_kana[mei_pic];
 
 //4桁パスワード生成
@@ -21,6 +21,6 @@ document.forms.ttg100.elements.PWD.value = document.forms.ttg100.elements.PWD_CN
 //名前入力
 document.forms.ttg100.elements.APLCT_FIRST_NAME.value = sei;
 document.forms.ttg100.elements.APLCT_LAST_NAME.value = mei;
-//document.forms.ttg100.elements.APLCT_FIRST_NAME_KANA.value = sei_kana;
+document.forms.ttg100.elements.APLCT_FIRST_NAME_KANA.value = sei_kana;
 document.forms.ttg100.elements.APLCT_LAST_NAME_KANA.value = mei_kana;
 
