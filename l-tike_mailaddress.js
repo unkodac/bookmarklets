e798e5aed55953a237ff95c508dee440b41464b9
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-12 16:33:10 +0900

    Update l-tike_mailaddress.js

--- l-tike_mailaddress.js
+++ l-tike_mailaddress.js
@@ -4,6 +4,6 @@
 const ketasuu = Math.floor(Math.random()*Math.floor(5))
 
 document.forms.ttg160.elements.MAIL_ADDRS.value=document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-1*ketasuu+5)+"@qrnail.com";
-document.forms.ttg160.elements.TEL.value=document.forms.ttg160.elements.TEL_CONFIRM.value="080"+(Math.floor(Math.random()*Math.floor(80000000))+10000000);
+document.forms.ttg160.elements.TEL.value=document.forms.ttg160.elements.TEL_CONFIRM.value="0"+(Math.floor(Math.random()*Math.floor(2))+7)+"0"+(Math.floor(Math.random()*Math.floor(80000000))+10000000);
 
 document.querySelector("[name=NEXT]").click(); //次ページへ
