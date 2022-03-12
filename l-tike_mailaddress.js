8a42cf2d28e4f3de59eb3f634ee5a6b37006f24a
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-13 01:11:49 +0900

    Update l-tike_mailaddress.js

--- l-tike_mailaddress.js
+++ l-tike_mailaddress.js
@@ -1,9 +1,9 @@
 //ローチケメアド電話番号自動入力[共用]
 
 //const ketasuu = 10; //メアドの桁数を指定
-const ketasuu = Math.floor(Math.random()*Math.floor(5))
+const ketasuu = Math.floor(Math.random()*Math.floor(6,11));
 
-document.forms.ttg160.elements.MAIL_ADDRS.value=document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-1*ketasuu+5)+"@qrnail.com";
+document.forms.ttg160.elements.MAIL_ADDRS.value=document.forms.ttg160.elements.MAIL_ADDRS_CONFIRM.value= Math.random().toString(36).slice(-1*ketasuu)+"@qrnail.com";
 document.forms.ttg160.elements.TEL.value=document.forms.ttg160.elements.TEL_CONFIRM.value="0"+(Math.floor(Math.random()*Math.floor(3))+7)+"0"+(Math.floor(Math.random()*Math.floor(80000000))+10000000);
 
 document.querySelector("[name=NEXT]").click(); //次ページへ
