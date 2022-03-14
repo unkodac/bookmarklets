9474b16f3785afb4878660f1e5ec633c936e2555
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-14 20:20:33 +0900

    Update l-tike_log.js

--- l-tike_log.js
+++ l-tike_log.js
@@ -2,15 +2,33 @@
 
 //申込履歴要素抽出
 var el = document.querySelectorAll("table td,th,p");
-var nichiji = el[6].outerText;
-var maisuu = el[11].outerText;
-var meado = el[14].outerText;
-var denwa = el[16].outerText;
-var uketori = el[17].outerText;
-var shiharai = el[20].outerText;
-var shimei = el[23].outerText;
-var shimei_kana = el[25].outerText;
-var pass = el[26].outerText;
+var a = el[5].outerText;
+var a_ = a.split('\n');
+var nichiji = a_.join('');
+var b = el[10].outerText;
+var b_ = b.split('\n');
+var maisuu = b_.join('');
+var c = el[14].outerText;
+var c_ = c.split('\n');
+var meado = c_.join('');
+var d = el[16].outerText;
+var d_ = d.split('\n');
+var denwa = d_.join('');
+var e = el[17].outerText;
+var e_ = e.split('\n');
+var uketori = e_.join('');
+var f = el[20].outerText;
+var f_ = f.split('\n');
+var shiharai = f_.join('');
+var g = el[23].outerText;
+var g_ = g.split('\n');
+var shimei = g_.join('');
+var h = el[25].outerText;
+var h_ = h.split('\n');
+var shimei_kana = h_.join('');
+var i = el[26].outerText;
+var i_ = i.split('\n');
+var pass = i_.join('');
 
 //ファイル名(メアド+txt)
 var text_name = [meado,'.txt'];
