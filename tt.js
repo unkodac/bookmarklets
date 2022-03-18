7189c6a7f151f6f4228784b307311591fab4ec05
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-18 13:36:29 +0900

    Create tt.js

---
+++ tt.js
@@ -0,0 +1,6 @@
+var log = 'unko';
+var text_name = 'a.txt';
+var blob = new Blob([log],{type:"text/plan"});
+var link = document.createElement('a');
+link.href = URL.createObjectURL(blob);
+link.download = text_name;link.click();
