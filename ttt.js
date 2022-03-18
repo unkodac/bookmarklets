77d61b0dc1eb381c3f9f6823d0ebd5d0a6c22cb6
unkodac <101387773+unkodac@users.noreply.github.com>
2022-03-18 13:39:07 +0900

    Update and rename tt.js to ttt.js

--- ttt.js
+++ ttt.js
@@ -1,5 +1,5 @@
 var log = 'unko';
-var text_name = 'a.txt';
+var text_name = 'a'+'.txt';
 var blob = new Blob([log],{type:"text/plan"});
 var link = document.createElement('a');
 link.href = URL.createObjectURL(blob);
