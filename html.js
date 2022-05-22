a1f43f042537d7075853ddf062b90fa3cc7d0789
unkodac <101387773+unkodac@users.noreply.github.com>
2022-05-22 19:05:22 +0900

    Update html.js

--- html.js
+++ html.js
@@ -4,3 +4,8 @@ for( var i=0 ; i < array.length; i++){
     var a = array[i].outerHTML;
     artist.push(a);
 }
+var text_name = 'm.txt';
+var blob = new Blob([artist],{type:"text/plan"});
+var link = document.createElement('a');
+link.href = URL.createObjectURL(blob);
+link.download = text_name;link.click();
