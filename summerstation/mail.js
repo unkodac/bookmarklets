8c2518af8d9c151a57e2e1cbb1232aa50b4bf378
unkodac <101387773+unkodac@users.noreply.github.com>
2022-06-27 11:29:20 +0900

    Create mail.js

---
+++ summerstation/mail.js
@@ -0,0 +1,13 @@
+//使用ドメイン入力
+var domains = ['@qrnail.com'];
+
+//メアド生成
+var ketasuu = Math.floor(Math.random()*6);
+var mail = Math.random().toString(36).slice(-1*(ketasuu+5)) + domains;
+
+//データ入力
+document.getElementsByName("mailaddress")[0].value = mail;
+document.getElementsByName("agree")[0].checked = true;
+
+//次画面へ
+document.querySelector("[id=submit_btn]").click();　
