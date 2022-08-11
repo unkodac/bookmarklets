3b66982ccc28e10e585d572c4b8f1434beefb8e7
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-11 19:53:08 +0900

    Create Tjoy-umeda.js

---
+++ Tjoy-umeda.js
@@ -0,0 +1,10 @@
+//Tジョイ梅田自動購入
+
+//座席選択
+document.querySelector("[id=A-12]").click();
+document.querySelector("[id=A-13]").click();
+document.querySelector("[id=A-14]").click();
+document.querySelector("[id=A-15]").click();
+
+//次画面
+document.querySelector("[id=seatChoice]").click();
