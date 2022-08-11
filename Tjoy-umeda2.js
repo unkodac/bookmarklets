225151cad774f5719572557ef3a6441fc0bba8f5
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-11 20:46:08 +0900

    Create Tjoy-umeda2.js

---
+++ Tjoy-umeda2.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("form-control")[0].value = "111111111111";
+document.getElementsByClassName("form-control control-date d-inline-block")[0].value = "02";
+document.getElementsByClassName("form-control control-date d-inline-block")[1].value = "23";
+document.getElementsByClassName("form-control control-security-code mb-1")[0].value = "456";
+document.querySelector("[id=seatPayment]").click();
