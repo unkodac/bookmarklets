d0cc4f87499ba6ac0e9ebd634ef4ebfef329e638
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-15 13:27:11 +0900

    Update Tjoy-umeda2.js

--- Tjoy-umeda2.js
+++ Tjoy-umeda2.js
@@ -1,10 +1,10 @@
 var el = document.querySelectorAll("p");
 var i = 0;
-while(i<100){
+while(i<1000){
   var html = el[i].outerHTML;
   if(html.includes('C3011')){
     var pass = html.match(/location.href ='(.*)'" class=/);
-    console.log(html);
+    document.location.href = pass[1];
     break;
   }
   i++;
