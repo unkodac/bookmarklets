23f4a4325a5061f3b2948a9da9783bf824312ca6
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-15 15:20:17 +0900

    Update Tjoy-umeda2.js

--- Tjoy-umeda2.js
+++ Tjoy-umeda2.js
@@ -1,9 +1,13 @@
 var el = document.querySelectorAll("p");
 var i = 0;
+var ofset = 0;
 while(i<1000){
   var html = el[i].outerHTML;
   if(html.includes('C3011')){
-    var pass = html.match(/shinjuku_wald9(.*)type=film);
+    ofset++;
+  }
+  if(ofset==2){
+    var pass = html.match(/shinjuku_wald9(.*)type=film/);
     document.location.href = pass[0];
     break;
   }
