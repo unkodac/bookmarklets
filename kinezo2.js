b3813d1841270a54f712fd704b1170fcc712973f
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-16 15:44:03 +0900

    Update and rename Tjoy-umeda.js to kinezo2.js

---
+++ kinezo2.js
@@ -0,0 +1,5 @@
+document.querySelector("[id=A-3]").click();
+document.querySelector("[id=A-4]").click();
+document.querySelector("[id=A-5]").click();
+document.querySelector("[id=A-6]").click();
+document.getElementById("seatChoice").click();
