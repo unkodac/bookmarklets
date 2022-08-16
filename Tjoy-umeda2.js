e41298c8b46c0be4eca4a1eebb901cb3e06d1a6e
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-16 11:52:07 +0900

    Update Tjoy-umeda2.js

--- Tjoy-umeda2.js
+++ Tjoy-umeda2.js
@@ -1,12 +1,13 @@
+document.querySelector("[id=event-tab]").click();
 var el = document.querySelectorAll("p");
 var i = 0;
-var ofset = 0;
+var offset = 0;
 while(i<1000){
   var html = el[i].outerHTML;
   if(html.includes('C3011')){
-    ofset++;
+    offset++;
   }
-  if(ofset==2){
+  if(offset==1){
     var pass = html.match(/shinjuku_wald9(.*)type=film/);
     document.location.href = pass[0];
     break;
