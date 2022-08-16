0b9aa494e6bb217b11f62b10a0bed0acf83ad3e9
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-16 21:04:48 +0900

    Update kinezo2.js

--- kinezo2.js
+++ kinezo2.js
@@ -43,5 +43,44 @@ if(document.getElementById("B-18").outerHTML.includes('seat-select')){
   document.querySelector("[id=A-12]").click();
 }
 
+/*
+if(document.getElementById("B-19").outerHTML.includes('seat-select')){
+  document.querySelector("[id=B-19]").click();
+}else if(document.getElementById("B-20").outerHTML.includes('seat-select')){
+  document.querySelector("[id=B-20]").click();
+}else if(document.getElementById("B-21").outerHTML.includes('seat-select')){
+  document.querySelector("[id=B-21]").click();
+}else if(document.getElementById("B-22").outerHTML.includes('seat-select')){
+  document.querySelector("[id=B-22]").click();
+}else if(document.getElementById("B-23").outerHTML.includes('seat-select')){
+  document.querySelector("[id=B-23]").click();
+}else if(document.getElementById("B-24").outerHTML.includes('seat-select')){
+  document.querySelector("[id=B-24]").click();
+}else if(document.getElementById("C-19").outerHTML.includes('seat-select')){
+  document.querySelector("[id=C-19]").click();
+}else if(document.getElementById("C-20").outerHTML.includes('seat-select')){
+  document.querySelector("[id=C-20]").click();
+}else if(document.getElementById("C-21").outerHTML.includes('seat-select')){
+  document.querySelector("[id=C-21]").click();
+}else if(document.getElementById("C-22").outerHTML.includes('seat-select')){
+  document.querySelector("[id=C-22]").click();
+}else if(document.getElementById("C-23").outerHTML.includes('seat-select')){
+  document.querySelector("[id=C-23]").click();
+}else if(document.getElementById("C-24").outerHTML.includes('seat-select')){
+  document.querySelector("[id=C-24]").click();
+}else if(document.getElementById("A-19").outerHTML.includes('seat-select')){
+  document.querySelector("[id=A-19]").click();
+}else if(document.getElementById("A-20").outerHTML.includes('seat-select')){
+  document.querySelector("[id=A-20]").click();
+}else if(document.getElementById("A-21").outerHTML.includes('seat-select')){
+  document.querySelector("[id=A-21]").click();
+}else if(document.getElementById("A-22").outerHTML.includes('seat-select')){
+  document.querySelector("[id=A-22]").click();
+}else if(document.getElementById("A-23").outerHTML.includes('seat-select')){
+  document.querySelector("[id=A-23]").click();
+}else if(document.getElementById("A-24").outerHTML.includes('seat-select')){
+  document.querySelector("[id=A-24]").click();
+}
+*/
 //次画面へ
 document.getElementById("seatChoice").click();
