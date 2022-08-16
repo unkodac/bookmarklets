a0b8098e95acaec135906d27c13d08f0cc704383
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-16 17:40:06 +0900

    Update kinezo2.js

--- kinezo2.js
+++ kinezo2.js
@@ -1,5 +1,33 @@
+//第1希望
+var seat1 = "B-18";
+//第2希望
+var seat2 = "B-17";
+//第3希望
+var seat3 = "B-16";
+//第4希望
+var seat4 = "C-18";
+//第5希望
+var seat5 = "C-17";
+//第6希望
+var seat6 = "C-16";
+
+if(document.getElementById("seat1").includes('seat-select')){
+  console.log(1);
+}else{
+  console.log(2);
+}
 document.querySelector("[id=A-3]").click();
 document.querySelector("[id=A-4]").click();
 document.querySelector("[id=A-5]").click();
 document.querySelector("[id=A-6]").click();
 document.getElementById("seatChoice").click();
+
+
+
+
+var check = document.getElementById("A-10");
+if(check.includes('seat-select')){
+  console.log(1);
+}else{
+  console.log(2);
+}
