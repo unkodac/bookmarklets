86702f8e9208763489fb0fb7b2fdacfa3624b5ed
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-16 13:26:44 +0900

    Update and rename Tjoy-umeda2.js to kinezo1.js

--- kinezo1.js
+++ kinezo1.js
@@ -1,14 +1,13 @@
-document.querySelector("[id=event-tab]").click();
 var el = document.querySelectorAll("p");
 var i = 0;
 var offset = 0;
 while(i<1000){
   var html = el[i].outerHTML;
-  if(html.includes('C3011')){
+  if(html.includes('type=event')){
     offset++;
   }
-  if(offset==1){
-    var pass = html.match(/shinjuku_wald9(.*)type=film/);
+  if(offset==2){
+    var pass = html.match(/shinjuku_wald9(.*)type=event/);
     document.location.href = pass[0];
     break;
   }
