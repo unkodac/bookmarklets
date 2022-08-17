90bc2b3a13d7bf8084396676e2e4bb364df118a6
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-17 11:16:27 +0900

    Update kinezo1.js

--- kinezo1.js
+++ kinezo1.js
@@ -1,15 +1,18 @@
-var el = document.querySelectorAll("p");
+var el = document.querySelectorAll("h5,p");
 var i = 0;
 var offset = 0;
 while(i<1000){
-  var html = el[i].outerHTML;
-  if(html.includes('type=event')){
-    offset++;
-  }
-  if(offset==2){
-    var pass = html.match(/shinjuku_wald9(.*)type=event/);
-    document.location.href = pass[0];
-    break;
+  var text = el[i].outerText;
+  if(text.includes('ずっと')){
+    var html = el[i].outerHTML;
+    if(html.includes('type=event')){
+      offset++;
+    }
+    if(offset==2){
+      var pass = html.match(/shinjuku_wald9(.*)type=event/);
+      document.location.href = pass[0];
+      break;
+    }
   }
   i++;
 }
