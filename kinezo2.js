7fef6243657cb3df17eb5eeb31a6c27f95e67556
unkodac <101387773+unkodac@users.noreply.github.com>
2022-08-22 13:08:48 +0900

    Update kinezo2.js

--- kinezo2.js
+++ kinezo2.js
@@ -1,48 +1,53 @@
 //空席の場合選択
-if(document.getElementById("B-18").outerHTML.includes('seat-select')){
-  document.querySelector("[id=B-18]").click();
-}else if(document.getElementById("B-17").outerHTML.includes('seat-select')){
-  document.querySelector("[id=B-17]").click();
-}else if(document.getElementById("B-16").outerHTML.includes('seat-select')){
-  document.querySelector("[id=B-16]").click();
-}else if(document.getElementById("B-15").outerHTML.includes('seat-select')){
-  document.querySelector("[id=B-15]").click();
-}else if(document.getElementById("B-14").outerHTML.includes('seat-select')){
-  document.querySelector("[id=B-14]").click();
-}else if(document.getElementById("B-13").outerHTML.includes('seat-select')){
-  document.querySelector("[id=B-13]").click();
-}else if(document.getElementById("B-12").outerHTML.includes('seat-select')){
-  document.querySelector("[id=B-12]").click();
-}else if(document.getElementById("C-18").outerHTML.includes('seat-select')){
-  document.querySelector("[id=C-18]").click();
-}else if(document.getElementById("C-17").outerHTML.includes('seat-select')){
-  document.querySelector("[id=C-17]").click();
-}else if(document.getElementById("C-16").outerHTML.includes('seat-select')){
-  document.querySelector("[id=C-16]").click();
-}else if(document.getElementById("C-15").outerHTML.includes('seat-select')){
-  document.querySelector("[id=C-15]").click();
-}else if(document.getElementById("C-14").outerHTML.includes('seat-select')){
-  document.querySelector("[id=C-14]").click();
-}else if(document.getElementById("C-13").outerHTML.includes('seat-select')){
-  document.querySelector("[id=C-13]").click();
-}else if(document.getElementById("C-12").outerHTML.includes('seat-select')){
-  document.querySelector("[id=C-12]").click();
-}else if(document.getElementById("A-18").outerHTML.includes('seat-select')){
-  document.querySelector("[id=A-18]").click();
-}else if(document.getElementById("A-17").outerHTML.includes('seat-select')){
-  document.querySelector("[id=A-17]").click();
-}else if(document.getElementById("A-16").outerHTML.includes('seat-select')){
-  document.querySelector("[id=A-16]").click();
-}else if(document.getElementById("A-15").outerHTML.includes('seat-select')){
-  document.querySelector("[id=A-15]").click();
-}else if(document.getElementById("A-14").outerHTML.includes('seat-select')){
-  document.querySelector("[id=A-14]").click();
-}else if(document.getElementById("A-13").outerHTML.includes('seat-select')){
-  document.querySelector("[id=A-13]").click();
-}else if(document.getElementById("A-12").outerHTML.includes('seat-select')){
-  document.querySelector("[id=A-12]").click();
+if(document.getElementById("A-10") != null){
+  if(document.getElementById("A-18").outerHTML.includes('seat-select')){
+    document.querySelector("[id=A-18]").click();
+  }else if(document.getElementById("A-17").outerHTML.includes('seat-select')){
+    document.querySelector("[id=A-17]").click();
+  }else if(document.getElementById("A-16").outerHTML.includes('seat-select')){
+    document.querySelector("[id=A-16]").click();
+  }else if(document.getElementById("A-15").outerHTML.includes('seat-select')){
+    document.querySelector("[id=A-15]").click();
+  }else if(document.getElementById("A-14").outerHTML.includes('seat-select')){
+    document.querySelector("[id=A-14]").click();
+  }else if(document.getElementById("A-13").outerHTML.includes('seat-select')){
+    document.querySelector("[id=A-13]").click();
+  }else if(document.getElementById("A-12").outerHTML.includes('seat-select')){
+    document.querySelector("[id=A-12]").click();
+  }
+}else if(document.getElementById("B-10") != null){
+  if(document.getElementById("B-18").outerHTML.includes('seat-select')){
+    document.querySelector("[id=B-18]").click();
+  }else if(document.getElementById("B-17").outerHTML.includes('seat-select')){
+    document.querySelector("[id=B-17]").click();
+  }else if(document.getElementById("B-16").outerHTML.includes('seat-select')){
+    document.querySelector("[id=B-16]").click();
+  }else if(document.getElementById("B-15").outerHTML.includes('seat-select')){
+    document.querySelector("[id=B-15]").click();
+  }else if(document.getElementById("B-14").outerHTML.includes('seat-select')){
+    document.querySelector("[id=B-14]").click();
+  }else if(document.getElementById("B-13").outerHTML.includes('seat-select')){
+    document.querySelector("[id=B-13]").click();
+  }else if(document.getElementById("B-12").outerHTML.includes('seat-select')){
+    document.querySelector("[id=B-12]").click();
+  }
+}else if(document.getElementById("C-10") != null){
+  if(document.getElementById("C-18").outerHTML.includes('seat-select')){
+    document.querySelector("[id=C-18]").click();
+  }else if(document.getElementById("C-17").outerHTML.includes('seat-select')){
+    document.querySelector("[id=C-17]").click();
+  }else if(document.getElementById("C-16").outerHTML.includes('seat-select')){
+    document.querySelector("[id=C-16]").click();
+  }else if(document.getElementById("C-15").outerHTML.includes('seat-select')){
+    document.querySelector("[id=C-15]").click();
+  }else if(document.getElementById("C-14").outerHTML.includes('seat-select')){
+    document.querySelector("[id=C-14]").click();
+  }else if(document.getElementById("C-13").outerHTML.includes('seat-select')){
+    document.querySelector("[id=C-13]").click();
+  }else if(document.getElementById("C-12").outerHTML.includes('seat-select')){
+    document.querySelector("[id=C-12]").click();
+  }
 }
-
 /*
 if(document.getElementById("B-19").outerHTML.includes('seat-select')){
   document.querySelector("[id=B-19]").click();
