78c7932407ac79d0986f2032afc020b122f67446
unkodac <101387773+unkodac@users.noreply.github.com>
2022-09-12 03:19:34 +0900

    Create senchaku.js

---
+++ pia/senchaku.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName("ticketSelect__actionLabel")[0].click();
+const changeEvent = new Event('change');
+document.getElementsByName("Y15-seatdata-seat_size_1")[0].value = "2";
+document.getElementsByName("Y15-seatdata-seat_size_1")[0].dispatchEvent(changeEvent);
+document.getElementsByClassName("button button--next Y15-ticket-entry-btn-app")[1].click();
+document.getElementsByClassName("Y15-regular-btn Y15-agree-modal button button--next js-modal-innerFirstElement")[0].click();
