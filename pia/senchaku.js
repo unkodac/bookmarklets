7a4139f54c010dc7ea9fb2276d9b29bf6eb84a55
unkodac <101387773+unkodac@users.noreply.github.com>
2022-09-12 03:25:21 +0900

    Update senchaku.js

--- pia/senchaku.js
+++ pia/senchaku.js
@@ -4,3 +4,6 @@ document.getElementsByName("Y15-seatdata-seat_size_1")[0].value = "2";
 document.getElementsByName("Y15-seatdata-seat_size_1")[0].dispatchEvent(changeEvent);
 document.getElementsByClassName("button button--next Y15-ticket-entry-btn-app")[1].click();
 document.getElementsByClassName("Y15-regular-btn Y15-agree-modal button button--next js-modal-innerFirstElement")[0].click();
+
+document.getElementsByClassName("accordion__label")[1].click();
+document.getElementsByClassName("button button--next")[0].click();
