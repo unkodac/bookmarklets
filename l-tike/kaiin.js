abc0e98766eda530851529e566af6ff33a991148
unkodac <101387773+unkodac@users.noreply.github.com>
2022-09-13 17:45:12 +0900

    Create kaiin.js

---
+++ l-tike/kaiin.js
@@ -0,0 +1,14 @@
+//使用ドメイン入力
+var domains = ['@qrnail.com'];
+
+//メアド生成
+var domain = domains[Math.floor(Math.random()*domains.length)];
+var mail = Math.random().toString(36).slice(-1*(Math.floor(Math.random()*6) + 5)) + domain;
+
+//パスワード生成
+var pass = Math.random().toString(36).slice(-8);
+
+//データ入力
+document.getElementById("email").value = mail;
+document.getElementById("password").value = pass;
+document.getElementById("repassword").value = pass;
