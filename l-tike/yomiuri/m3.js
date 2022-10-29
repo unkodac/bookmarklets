572750e454e6d578f7a9090ac05185ea10bba9f5
unkodac <101387773+unkodac@users.noreply.github.com>
2022-10-29 15:37:03 +0900

    Update m3.js

--- l-tike/yomiuri/m3.js
+++ l-tike/yomiuri/m3.js
@@ -60,6 +60,6 @@ link.href = URL.createObjectURL(blob);
 link.download = text_name;link.click();*/
 
 //次画面へ
-setTimeout(function(){
+/*setTimeout(function(){
     document.querySelector("[name=ENTRY_FIX]").click();
-},1500);
+},1500);*/
