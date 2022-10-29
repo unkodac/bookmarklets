77daab6276225b10c9d41bb3be610f88f95b9891
unkodac <101387773+unkodac@users.noreply.github.com>
2022-10-29 18:33:24 +0900

    Update m4.js

--- l-tike/yomiuri/m4.js
+++ l-tike/yomiuri/m4.js
@@ -4,15 +4,4 @@ var user = "高森";
 
 //ログ本体生成
 var log = user;
-function copyToClipBoard(){
-  var anyText= log;
-  var textBox = document.createElement("textarea");
-  textBox.setAttribute("id", "target");
-  textBox.setAttribute("type", "hidden");
-  textBox.textContent = anyText;
-  document.body.appendChild(textBox);
-  textBox.select();
-  document.execCommand('copy');  
-  document.body.removeChild(textBox);
-}
-copyToClipBoard();
+navigator.clipboard.writeText(log);
