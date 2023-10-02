7e3073eaf980efd17037048b2eb2cc429dc3a7dc
unkodac <101387773+unkodac@users.noreply.github.com>
2023-10-02 20:57:25 +0900

    Update touken-link.js

--- l-tike/touken-link.js
+++ l-tike/touken-link.js
@@ -1,10 +1,10 @@
 var form = document.createElement('form');
 document.body.appendChild(form);
 form.method = 'post';
-form.action = 'https://w1.red.onlineticket.jp/sf/tkt101/detail/3827700002?P6=z07';
+form.action = 'https://l-tike.com/st1/97vmsbx56r22l16vwros';
 var input = document.createElement('input');
 input.type = 'hidden';
-input.name = 'keyword';
-input.value = '9261';
+input.name = 'CERK';
+input.value = '63b16b4469074e61580d96ed7458a572';
 form.appendChild(input);
 form.submit();
